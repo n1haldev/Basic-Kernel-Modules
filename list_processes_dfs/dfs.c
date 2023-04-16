b0c478730924250f73c4a789a15de4eacee6ebef
Nihal Developer <nihaltmdev@gmail.com>
2023-04-16 09:33:03 +0530

    printing more like a tree now

--- list_processes_dfs/dfs.c
+++ list_processes_dfs/dfs.c
@@ -2,29 +2,31 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("GPL");
 
-void dfs(struct task_struct *task) {
-	if(task==NULL)
-	return;	
-	
-	struct task_struct *child;
-	struct list_head *list;
-	
-	printk(KERN_INFO "PID: %d  Name: %s\n", task->pid, task->comm);
-	
-	list_for_each(list, &task->children) {
-		child=list_entry(list, struct task_struct, sibling);
-		dfs(child);
-	}
+void dfs(struct task_struct *task, int indent) {
+    struct task_struct *child;
+    struct list_head *list;
+    int i;
+
+    for (i = 0; i < indent; i++) {
+        printk(KERN_CONT "\t");
+    }
+    printk(KERN_CONT "|--%s[%d]\n", task->comm, task->pid);
+
+    list_for_each(list, &task->children) {
+        child = list_entry(list, struct task_struct, sibling);
+        dfs(child, indent + 1);
+    }
 }
 
 static int dfs_init(void) {
 	
 	printk(KERN_INFO "Listing all processes in DFS order:\n");
 	
-	dfs(&init_task);
+	dfs(&init_task, 0);
 	
 	return 0;
 }
